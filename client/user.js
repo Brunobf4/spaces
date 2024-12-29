46c2f60f4967fbc37190c271dd0640058ef8b74e
Bruno Ferreira Pedraça <brunoferreirapedraca@gmail.com>
2024-12-28 22:48:59 -0300

    Object assign

--- client/user.js
+++ client/user.js
@@ -9,6 +9,7 @@ const __dirname = dirname(__filename);
 
 export default class User {
   constructor(name, email, senha) {
+    Object.assign(this, { _name: name, _email: email, _senha: senha });
     this._name = name;
     this._email = email;
     this._senha = senha;
